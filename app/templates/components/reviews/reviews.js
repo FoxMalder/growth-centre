d2dc8bcdaa5dc3b019a90d548f851c9f585d0e15
Sergey Glazov <yaglazov@gmail.com>
2018-08-12 17:41:12 +0300

    фотка для отзывов, js для слайдера

---
+++ app/templates/components/reviews/reviews.js
@@ -0,0 +1,9 @@
+$(".reviews-list").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: !0,
+  arrows: !0,
+  speed: 280,
+  focusOnSelect: !0,
+  adaptiveHeight: !0
+});
---
+++ app/templates/components/slider/slider.js
@@ -0,0 +1,9 @@
+$(".sliders").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: !0,
+  arrows: !0,
+  speed: 280,
+  focusOnSelect: !0,
+  adaptiveHeight: !0
+});
