26bc98dd99fbb590e38e41c00bfa7e9aee8d4be7
Sergey Glazov <yaglazov@gmail.com>
2018-08-12 03:10:57 +0300

    Update 2018-08-12T00:10:55.189Z

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,14 +1,52 @@
+// подсвечиваем ссылки с одинаковым адресом
+// if ($) $(function () {
+//
+//   $('a').hover(function () {
+//     if ($(this).attr('href') != '' && $(this).attr('href') != '#') {
+//       $('a[href="' + $(this).attr('href') + '"]').addClass('hover');
+//     }
+//   }, function () {
+//     $('a').removeClass('hover');
+//   });
+//
+// });
+// /подсвечиваем ссылки с одинаковым адресом
+"use strict";
 'use strict';
 
-// подсвечиваем ссылки с одинаковым адресом
-if ($) $(function () {
+$(document).ready(function () {
+  var scrollerStart = false;
 
-  $('a').hover(function () {
-    if ($(this).attr('href') != '' && $(this).attr('href') != '#') {
-      $('a[href="' + $(this).attr('href') + '"]').addClass('hover');
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 0) {
+      $('#scroller').fadeIn();
+    } else {
+      $('#scroller').fadeOut();
     }
-  }, function () {
-    $('a').removeClass('hover');
+  });
+
+  $('#scroller').click(function () {
+    scrollerStart = true;
+    $('body,html').animate({ scrollTop: 0 }, 400, function () {
+      scrollerStart = false;
+    });
+    return false;
   });
 });
-// /подсвечиваем ссылки с одинаковым адресом
+'use strict';
+
+$(document).ready(function () {
+  var mainHeader = $('.header_wrap').outerHeight();
+  var menuWrap = $('.header-nav');
+
+  $(window).scroll(function () {
+
+    if ($(window).scrollTop() > mainHeader) {
+      menuWrap.css({ position: 'fixed' });
+      menuWrap.addClass('is-fixed');
+    } else {
+      menuWrap.css({ position: 'relative' });
+      menuWrap.removeClass('is-fixed');
+    }
+  });
+});
